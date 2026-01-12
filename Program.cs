3106edf0a18575d6641f01a9b20cdd4221970360
lenotmaul-ui <lenotmaul@gmail.com>
2026-01-12 16:48:06 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using KeyAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ERLANXITER
+{
+    internal static class Program
+    {
+
+        public static api KeyAuthApp;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
+        [System.Runtime.InteropServices.DllImport("shcore.dll")]
+        private static extern int SetProcessDpiAwareness(int value);
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                try
+                {
+                    SetProcessDpiAwareness(1); // System DPI Aware
+                }
+                catch
+                {
+                    SetProcessDPIAware(); // Fallback for older versions
+                }
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new From2());
+        }
+    }
+}
